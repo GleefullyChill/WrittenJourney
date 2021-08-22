2e40337f6e17e041b3158fed334bb66808f334ba
Your Name <you@example.com>
2021-08-22 00:57:12 +0000

    schema4

--- schema.sql
+++ schema.sql
@@ -8,33 +8,35 @@ DROP TABLE IF EXISTS story_contributions CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL
 );
 
  CREATE TABLE stories (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-
   title VARCHAR(255) NOT NULL,
-  abstract VARCHAR(255) NOT NULL);
+  abstract VARCHAR(255) NOT NULL,
+  complited BOOLEAN NOT NULL DEFAULT FALSE
+  );
 
 CREATE TABLE story_contributions (
   id SERIAL PRIMARY KEY NOT NULL,
   story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   contribution_id INTEGER REFERENCES contributions(id) ON DELETE CASCADE,
-  within_story BOOLEAN NOT NULL DEFAULT TRUE );
+  within_story BOOLEAN NOT NULL DEFAULT FALSE);
 
 
 CREATE TABLE contributions (
   id SERIAL PRIMARY KEY NOT NULL,
-  content VARCHAR(5000));
+  content VARCHAR(5000),
+  active BOOLEAN NOT NULL DEFAULT TRUE);
 
   CREATE TABLE votes (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  contribution_id INTEGER REFERENCES contributions(id) ON DELETE CASCADE);
+  contribution_id INTEGER REFERENCES contributions(id) ON DELETE CASCADE
+  flag_voted BOOLEAN NOT NULL DEFAULT FALSE);
 
   -- check -- if it is positive it means user vote his own contribution
   select * from votes v
