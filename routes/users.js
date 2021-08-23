78687f729a7c4c00edcb038d06a770f8ae8c8329
smile2682 <smilecrystal.liyi@gmail.com>
2021-08-23 05:49:36 +0000

    finish three routes.

--- routes/users.js
+++ routes/users.js
@@ -21,42 +21,6 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-
-  // do this instead
-// need to add coockie parser to server.js
-router.get('/login/:id', (req, res) => {
-  req.session.user_id = req.params.id;
-  res.redirect('/');
-});
-
-//create a story
-router.post("/create", function(req, res) {
-  if (!req.body.text) {
-    res.status(400).json({ error: 'invalid request: no data in POST body'});
-    return;
-  }
-// req.body.key defined in the ajax jquery?
-  const user = req.body.user;
-  const story = {
-    user: user,
-    content: {
-      text: req.body.text
-    },
-    abstract:req.body.abstract,
-    created_at: Date.now()
-  };
-
-  DataHelpers.saveStory(story, (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      // Jquery add?
-      res.status(201).send();
-    }
-  });
-});
-
-
   return router;
 };
 
