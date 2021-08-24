024ab6aa67ef1a08dc623d5dfc7ac62437cbd358
smile2682 <smilecrystal.liyi@gmail.com>
2021-08-24 02:25:13 +0000

    add changes

--- routes/upvote.js
+++ routes/upvote.js
@@ -6,20 +6,21 @@ module.exports = (db) => {
   const owner_id = req.session.user_id;
   const contribution_id = req.prams.contribution_id;
   // const numVotes = 0; this needs to be added
-  router.post("/:contribution/upvote", function (req, res) {
+  router.post("/", function (req, res) {
     // Firstly, check if the user has already voted. If flag_voted is true, update to make it false and return the votes number to update.
     db.query(`SELECT flag_voted FROM votes WHERE owner_id = $1 AND contribution_id = $2`[owner_id, contribution_id])
       .then((result) => { return result.rows[0].flag_voted })
       .then((upvote) => {
         if (upvote === true) {
           db.query(`UPDATE votes SET upvote = false WHERE owner_id = $1`[owner_id])
+          // use select count contribution id
           return numOfVotes;
-        }
-      })
+
       // function to update the number of votes
       .then()
       // function like changing the heart sign from red to white inside the below .then
       .then()
+    }
 
 
     // secondly, the returned is empty, meaning the user has not voted
@@ -41,7 +42,7 @@ module.exports = (db) => {
     .then()
     // function like changing the heart sign from red to white inside the below .then
     .then()
-
+  })
   return router;
 });
 
