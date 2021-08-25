8ec0849c297ed8cd8a8b9946028f6caed2b25238
Luke <77421129+GleefullyChill@users.noreply.github.com>
2021-08-25 17:51:04 -0600

    Update createStoryButton.js

--- public/scripts/createStoryButton.js
+++ public/scripts/createStoryButton.js
@@ -7,6 +7,7 @@ $(() => {
   $('.toggle-switch').on('click', ()=>{
   $(".create-story").slideToggle()
   })
+})
   // const $createStoryform = $("#create-story");
   // $createStoryform.on('submit', function (event) {
   //   // this prevents html to defaultly post to another page
