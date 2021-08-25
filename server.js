2a29794b8841352e6bc41584afe5e8f43688610d
smile2682 <smilecrystal.liyi@gmail.com>
2021-08-25 17:28:44 +0000

    add login routes

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ const widgetsRoutes = require("./routes/widgets");
 const titlesRoutes = require('./routes/titles')
 const storyRoutes = require('./routes/story')
 const createStoryRoute = require('./routes/createStory')
+const loginRoutes = require('./routes/login')
 
 
 // Mount all resource routes
@@ -54,6 +55,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.use("/api/titles", titlesRoutes(db));
 //app.use("/story", storyRoutes(db));
 app.use("/create", createStoryRoute(db));
+app.use("/login/:user_id", loginRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
