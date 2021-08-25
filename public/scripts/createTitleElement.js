aac29a8f9f5e41128996424b6e7129e4f4420c03
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-24 21:02:05 -0600

    add a lot of debugging

--- public/scripts/createTitleElement.js
+++ public/scripts/createTitleElement.js
@@ -1,28 +1,25 @@
 
 //called in renderTitles.js and createStorySubmit.js
 
+const createTitleElement = function(title) {
 
-$(() => {
-
-
-  const createTitleElement = function(title) {
-    const $username = $(`<div>`).text(title.username);
-    const $titleText = $(`<h3>`).text(title.title);
-    const $abstract = $(`<p>`).text(title.abstract);
-    let $complete;
-    if (title.complete === true) {
-      $complete = $(`<div>${'Complete!'}</div>`);
-    } else {
-      $complete = $(`<div>${'In Progress'}</div>`);
-    }
-    const $title = $(`
-      <section class="title">
-        ${$titleText}
-        ${$abstract}
-        ${$username}
-        ${$complete}
-      </section>
-    `)
-    return $title
+  const $username = $(`<div>`).text(title.username);
+  const $titleText = $(`<h3>`).text(title.title);
+  const $abstract = $(`<p>`).text(title.abstract);
+  const $storyId = $(`<div class="story-id">`).text(title.story_id)
+  let $complete;
+  if (title.complete === true) {
+    $complete = $(`<div>${'Complete!'}</div>`);
+  } else {
+    $complete = $(`<div>${'In Progress'}</div>`);
   }
-})
+  $titleText.addClass("render-story-button")
+  const $title = $(`<section class="title">`).append(
+    $titleText,
+    $abstract,
+    $username,
+    $complete,
+    $storyId
+  )
+  return $title
+};
