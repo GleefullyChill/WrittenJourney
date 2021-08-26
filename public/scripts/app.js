9d2f89ce290a4f737b475cab8867a7f99c1db39e
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-26 13:19:41 -0600

    delete empty space and dead code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,7 @@
 
+
+//this will make it client side SPA behaviour
+
 const loadTitles = function() {
   $.get("/api/titles", (response) => {
     renderTitles(response);
@@ -32,39 +35,9 @@ const addContributionToStory = function(storyId, contributionId) {
 }
 
 $(() => {
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($("body"));
-  //   }
-  // });;
 
-  //this will make it client side SPA behaviour
+  // Once everything's loaded, start it up!
 
   loadTitles()
 
-
-
-  // $.post(/*route here*/)
-   //  .then((/*what the route returns*/) => {})
-
-  // $.post(/*route here*/)
-  //   .then((/*what the route returns*/) => {})
-
-  // $.post(/*route here*/)
-  //   .then((/*what the route returns*/) => {})
-
-  // $.ajax({
-  //   method: "PATCH",
-  //   url: "/api/users"
-
-  // $.ajax({
-  //   method: "PATCH",
-  //   url: "/api/users"
-
-  // $.ajax({
-  //   method: "PATCH",
-  //   url: "/api/users"
 });
--- routes/addContribution.js
+++ routes/addContribution.js
@@ -5,10 +5,6 @@ const router  = express.Router();
 //NOTE: this is a PATCH route, because it only updates, no creation of data
 module.exports = (db) => {
   router.patch("/", (req, res) => {
-    // console.log(
-    //   "req.params: ", req.params,
-    //   "req.body: ", req.body,
-    //   "req.data: ", req.header)
 
     const contributionId = req.body.contribution_id;
     const storyId = req.body.story_id;
